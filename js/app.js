8a0c27371941898a57b46282fdc2ac01d3f75e8a
Aravind Adithya <aravindadithya95@gmail.com>
2018-01-02 13:46:49 +0530

    Knockout model and view model

--- js/app.js
+++ js/app.js
@@ -1,22 +1,79 @@
 var map;
+var locations = [
+  {
+    title: 'QuickChek',
+    location: {
+      lat: 40.743759,
+      lng: -74.155682
+    }
+  },
+  {
+    title: '5 Grains Rice',
+    location: {
+      lat: 40.745999,
+      lng: -74.158852
+    }
+  },
+  {
+    title: 'Smashburger',
+    location: {
+      lat: 40.741283,
+      lng: -74.179654
+    }
+  },
+  {
+    title: 'The Oculus',
+    location: {
+      lat: 40.711399,
+      lng: -74.011146
+    }
+  },
+  {
+    title: 'Newport Centre',
+    location: {
+      lat: 40.727173,
+      lng: -74.036209
+    }
+  }
+];
 var markers = [];
 
+/*
+ * Knockout Model
+ */
+var Location = function(data) {
+  this.title = data.title;
+  this.location = data.location;
+}
+
+/*
+ * Knockout ViewModel
+ */
+var ViewModel = function() {
+  var self = this;
+
+  this.locationList = ko.observableArray([]);
+  this.filteredList = ko.observableArray([]);
+
+  locations.forEach(function(location) {
+    self.locationList.push(new Location(location));
+    self.filteredList.push(new Location(location));
+  });
+}
+
+ko.applyBindings(new ViewModel());
+
 function initMap() {
+  var center = {
+    lat: 40.7413549,
+    lng: -74.0900000
+  }
+
   // Constructor creates a new map.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7413549, lng: -74.0900000},
-    zoom: 13,
-    mapTypeControl: false,
-    fullscreenControl: false
+    center: center,
+    zoom: 13
   });
-
-  var locations = [
-    {title: 'QuickChek', location: {lat: 40.743759, lng: -74.155682}},
-    {title: '5 Grains Rice', location: {lat: 40.745999, lng: -74.158852}},
-    {title: 'Smashburger', location: {lat: 40.741283, lng: -74.179654}},
-    {title: 'The Oculus', location: {lat: 40.711399, lng: -74.011146}},
-    {title: 'Newport Centre', location: {lat: 40.727173, lng: -74.036209}}
-  ];
 
   var largeInfoWindow = new google.maps.InfoWindow();
   var bounds = new google.maps.LatLngBounds();
